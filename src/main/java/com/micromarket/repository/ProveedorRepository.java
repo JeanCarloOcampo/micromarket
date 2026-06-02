81dcc1cc4150e95e0f2774d65ed73f49b88b6aa5
devstiven12 <devstiven12@gmail.com>
2026-06-01 20:26:29 -0500

    feat: complete CRUD for suppliers with unique NIT and warehouse entry

---
+++ src/main/java/com/micromarket/repository/ProveedorRepository.java
@@ -0,0 +1,12 @@
+package com.micromarket.repository;
+
+import com.micromarket.entity.Proveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface ProveedorRepository extends JpaRepository<Proveedor, Long> {
+
+    Optional<Proveedor> findByNit(String nit);
+}
